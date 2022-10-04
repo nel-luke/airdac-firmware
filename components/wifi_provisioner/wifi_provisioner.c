7682568200244c2b4e0ab4dc90955d05f46ca02d
Luke Nel <lukaznel@gmail.com>
2022-10-04 16:25:10 +0200

    Add wifi_provisioner component
    Component starts Wi-Fi AP that redirects to a captive portal.

---
+++ components/wifi_provisioner/wifi_provisioner.c
@@ -0,0 +1,346 @@
+#include "wifi_provisioner.h"
+
+#include <sys/param.h>
+
+#include "esp_log.h"
+#include "esp_system.h"
+
+#include <esp_wifi.h>
+#include <esp_netif.h>
+#include <esp_http_server.h>
+
+#include <lwip/inet.h>
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+
+#define DNS_PORT (53)
+#define DNS_MAX_LEN (256)
+
+#define OPCODE_MASK (0x7800)
+#define QR_FLAG (1 << 7)
+#define QD_TYPE_A (0x0001)
+#define ANS_TTL_SEC (300)
+
+static const char *TAG = "wifi_provisioner";
+
+extern const char root_start[] asm("_binary_root_html_start");
+extern const char root_end[] asm("_binary_root_html_end");
+
+// DNS Header Packet
+typedef struct __attribute__((__packed__))
+{
+    uint16_t id;
+    uint16_t flags;
+    uint16_t qd_count;
+    uint16_t an_count;
+    uint16_t ns_count;
+    uint16_t ar_count;
+} dns_header_t;
+
+// DNS Question Packet
+typedef struct {
+    uint16_t type;
+    uint16_t class;
+} dns_question_t;
+
+// DNS Answer Packet
+typedef struct __attribute__((__packed__))
+{
+    uint16_t ptr_offset;
+    uint16_t type;
+    uint16_t class;
+    uint32_t ttl;
+    uint16_t addr_len;
+    uint32_t ip_addr;
+} dns_answer_t;
+
+/*
+    Parse the name from the packet from the DNS name format to a regular .-seperated name
+    returns the pointer to the next part of the packet
+*/
+static char *parse_dns_name(char *raw_name, char *parsed_name, size_t parsed_name_max_len)
+{
+
+    char *label = raw_name;
+    char *name_itr = parsed_name;
+    int name_len = 0;
+
+    do {
+        int sub_name_len = *label;
+        // (len + 1) since we are adding  a '.'
+        name_len += (sub_name_len + 1);
+        if (name_len > parsed_name_max_len) {
+            return NULL;
+        }
+
+        // Copy the sub name that follows the the label
+        memcpy(name_itr, label + 1, sub_name_len);
+        name_itr[sub_name_len] = '.';
+        name_itr += (sub_name_len + 1);
+        label += sub_name_len + 1;
+    } while (*label != 0);
+
+    // Terminate the final string, replacing the last '.'
+    parsed_name[name_len - 1] = '\0';
+    // Return pointer to first char after the name
+    return label + 1;
+}
+
+// Parses the DNS request and prepares a DNS response with the IP of the softAP
+static int parse_dns_request(char *req, size_t req_len, char *dns_reply, size_t dns_reply_max_len)
+{
+    if (req_len > dns_reply_max_len) {
+        return -1;
+    }
+
+    // Prepare the reply
+    memset(dns_reply, 0, dns_reply_max_len);
+    memcpy(dns_reply, req, req_len);
+
+    // Endianess of NW packet different from chip
+    dns_header_t *header = (dns_header_t *)dns_reply;
+    ESP_LOGD(TAG, "DNS query with header id: 0x%X, flags: 0x%X, qd_count: %d",
+             ntohs(header->id), ntohs(header->flags), ntohs(header->qd_count));
+
+    // Not a standard query
+    if ((header->flags & OPCODE_MASK) != 0) {
+        return 0;
+    }
+
+    // Set question response flag
+    header->flags |= QR_FLAG;
+
+    uint16_t qd_count = ntohs(header->qd_count);
+    header->an_count = htons(qd_count);
+
+    int reply_len = qd_count * sizeof(dns_answer_t) + req_len;
+    if (reply_len > dns_reply_max_len) {
+        return -1;
+    }
+
+    // Pointer to current answer and question
+    char *cur_ans_ptr = dns_reply + req_len;
+    char *cur_qd_ptr = dns_reply + sizeof(dns_header_t);
+    char name[128];
+
+    // Respond to all questions with the ESP32's IP address
+    for (int i = 0; i < qd_count; i++) {
+        char *name_end_ptr = parse_dns_name(cur_qd_ptr, name, sizeof(name));
+        if (name_end_ptr == NULL) {
+            ESP_LOGE(TAG, "Failed to parse DNS question: %s", cur_qd_ptr);
+            return -1;
+        }
+
+        dns_question_t *question = (dns_question_t *)(name_end_ptr);
+        uint16_t qd_type = ntohs(question->type);
+        uint16_t qd_class = ntohs(question->class);
+
+        ESP_LOGD(TAG, "Received type: %d | Class: %d | Question for: %s", qd_type, qd_class, name);
+
+        if (qd_type == QD_TYPE_A) {
+            dns_answer_t *answer = (dns_answer_t *)cur_ans_ptr;
+
+            answer->ptr_offset = htons(0xC000 | (cur_qd_ptr - dns_reply));
+            answer->type = htons(qd_type);
+            answer->class = htons(qd_class);
+            answer->ttl = htonl(ANS_TTL_SEC);
+
+            esp_netif_ip_info_t ip_info;
+            esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_AP_DEF"), &ip_info);
+            ESP_LOGD(TAG, "Answer with PTR offset: 0x%X and IP 0x%X", ntohs(answer->ptr_offset), ip_info.ip.addr);
+
+            answer->addr_len = htons(sizeof(ip_info.ip.addr));
+            answer->ip_addr = ip_info.ip.addr;
+        }
+    }
+    return reply_len;
+}
+
+/*
+    Sets up a socket and listen for DNS queries,
+    replies to all type A queries with the IP of the softAP
+*/
+void dns_server_task(void *pvParameters)
+{
+    char rx_buffer[128];
+    char addr_str[128];
+    int addr_family;
+    int ip_protocol;
+
+    while (1) {
+
+        struct sockaddr_in dest_addr;
+        dest_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+        dest_addr.sin_family = AF_INET;
+        dest_addr.sin_port = htons(DNS_PORT);
+        addr_family = AF_INET;
+        ip_protocol = IPPROTO_IP;
+        inet_ntoa_r(dest_addr.sin_addr, addr_str, sizeof(addr_str) - 1);
+
+        int sock = socket(addr_family, SOCK_DGRAM, ip_protocol);
+        if (sock < 0) {
+            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            break;
+        }
+        ESP_LOGI(TAG, "Socket created");
+
+        int err = bind(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
+        if (err < 0) {
+            ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
+        }
+        ESP_LOGI(TAG, "Socket bound, port %d", DNS_PORT);
+
+        while (1) {
+            ESP_LOGI(TAG, "Waiting for data");
+            struct sockaddr_in6 source_addr; // Large enough for both IPv4 or IPv6
+            socklen_t socklen = sizeof(source_addr);
+            int len = recvfrom(sock, rx_buffer, sizeof(rx_buffer) - 1, 0, (struct sockaddr *)&source_addr, &socklen);
+
+            // Error occurred during receiving
+            if (len < 0) {
+                ESP_LOGE(TAG, "recvfrom failed: errno %d", errno);
+                close(sock);
+                break;
+            }
+                // Data received
+            else {
+                // Get the sender's ip address as string
+                if (source_addr.sin6_family == PF_INET) {
+                    inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr.s_addr, addr_str, sizeof(addr_str) - 1);
+                } else if (source_addr.sin6_family == PF_INET6) {
+                    inet6_ntoa_r(source_addr.sin6_addr, addr_str, sizeof(addr_str) - 1);
+                }
+
+                // Null-terminate whatever we received and treat like a string...
+                rx_buffer[len] = 0;
+
+                char reply[DNS_MAX_LEN];
+                int reply_len = parse_dns_request(rx_buffer, len, reply, DNS_MAX_LEN);
+
+                ESP_LOGI(TAG, "Received %d bytes from %s | DNS reply with len: %d", len, addr_str, reply_len);
+                if (reply_len <= 0) {
+                    ESP_LOGE(TAG, "Failed to prepare a DNS reply");
+                } else {
+                    int err = sendto(sock, reply, reply_len, 0, (struct sockaddr *)&source_addr, sizeof(source_addr));
+                    if (err < 0) {
+                        ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
+                        break;
+                    }
+                }
+            }
+        }
+
+        if (sock != -1) {
+            ESP_LOGE(TAG, "Shutting down socket");
+            shutdown(sock, 0);
+            close(sock);
+        }
+    }
+    vTaskDelete(NULL);
+}
+
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data)
+{
+    if (event_id == WIFI_EVENT_AP_STACONNECTED) {
+        wifi_event_ap_staconnected_t *event = (wifi_event_ap_staconnected_t *)event_data;
+        ESP_LOGI(TAG, "station " MACSTR " join, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    } else if (event_id == WIFI_EVENT_AP_STADISCONNECTED) {
+        wifi_event_ap_stadisconnected_t *event = (wifi_event_ap_stadisconnected_t *)event_data;
+        ESP_LOGI(TAG, "station " MACSTR " leave, AID=%d",
+                 MAC2STR(event->mac), event->aid);
+    }
+}
+
+static void wifi_init_softap(const char* host_name)
+{
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL));
+
+    wifi_config_t wifi_config = {
+            .ap = {
+                    .ssid = "",
+                    .ssid_len = strlen(host_name),
+                    .max_connection = 5,
+                    .authmode = WIFI_AUTH_OPEN
+            },
+    };
+    strcpy((char*)wifi_config.ap.ssid, host_name);
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    esp_netif_ip_info_t ip_info;
+    esp_netif_get_ip_info(esp_netif_get_handle_from_ifkey("WIFI_AP_DEF"), &ip_info);
+
+    char ip_addr[16];
+    inet_ntoa_r(ip_info.ip.addr, ip_addr, 16);
+    ESP_LOGI(TAG, "Set up softAP with IP: %s", ip_addr);
+}
+
+// HTTP GET Handler
+static esp_err_t root_get_handler(httpd_req_t *req)
+{
+    const uint32_t root_len = root_end - root_start;
+
+    ESP_LOGI(TAG, "Serve root");
+    httpd_resp_set_type(req, "text/html");
+    httpd_resp_send(req, root_start, root_len);
+
+    return ESP_OK;
+}
+
+static const httpd_uri_t root = {
+        .uri = "/",
+        .method = HTTP_GET,
+        .handler = root_get_handler
+};
+
+// HTTP Error (404) Handler - Redirects all requests to the root page
+esp_err_t http_404_error_handler(httpd_req_t *req, httpd_err_code_t err)
+{
+    // Set status
+    httpd_resp_set_status(req, "302 Temporary Redirect");
+    // Redirect to the "/" root directory
+    httpd_resp_set_hdr(req, "Location", "/");
+    // iOS requires content in the response to detect a captive portal, simply redirecting is not sufficient.
+    httpd_resp_send(req, "Redirect to the captive portal", HTTPD_RESP_USE_STRLEN);
+
+    ESP_LOGI(TAG, "Redirecting to root");
+    return ESP_OK;
+}
+
+static httpd_handle_t start_webserver(void)
+{
+    httpd_handle_t server = NULL;
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    config.max_open_sockets = 5;
+    config.lru_purge_enable = true;
+
+    // Start the httpd server
+    ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
+    if (httpd_start(&server, &config) == ESP_OK) {
+        // Set URI handlers
+        ESP_LOGI(TAG, "Registering URI handlers");
+        httpd_register_uri_handler(server, &root);
+        httpd_register_err_handler(server, HTTPD_404_NOT_FOUND, http_404_error_handler);
+    }
+    return server;
+}
+
+void start_provisioner(const char* host_name) {
+    // Initialise ESP32 in SoftAP mode
+    wifi_init_softap(host_name);
+
+    // Start the server for the first time
+    start_webserver();
+
+    // Start the DNS server that will redirect all queries to the softAP IP
+    xTaskCreate(dns_server_task, "dns_server", 4096, NULL, 5, NULL);
+}
--- main/airdac_firmware.c
+++ main/airdac_firmware.c
@@ -1,10 +1,42 @@
 #include <esp_event.h>
-
 #include <esp_log.h>
+#include <esp_system.h>
+
+#include <nvs_flash.h>
+#include <esp_wifi.h>
+#include <esp_netif.h>
+#include <esp_http_server.h>
+
+#include "wifi_provisioner.h"
+
 static const char *TAG = "app";
+#define HOST_NAME "AirDAC"
 
 void app_main(void)
 {
+    /*
+        Turn off warnings from HTTP server as redirecting traffic will yield
+        lots of invalid requests
+    */
+    esp_log_level_set("httpd_uri", ESP_LOG_ERROR);
+    esp_log_level_set("httpd_txrx", ESP_LOG_ERROR);
+    esp_log_level_set("httpd_parse", ESP_LOG_ERROR);
+
+
+    // Initialize networking stack
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    // Create default event loop needed by the  main app
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+    // Initialize NVS needed by Wi-Fi
+    ESP_ERROR_CHECK(nvs_flash_init());
+
+    // Initialize Wi-Fi including netif with default config
+    esp_netif_create_default_wifi_ap();
+
+    start_provisioner(HOST_NAME);
+
     while (1) {
         ESP_LOGI(TAG, "Hello world!");
         vTaskDelay(1000 / portTICK_PERIOD_MS);
